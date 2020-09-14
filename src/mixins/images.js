62ed8e96346c2a65f13cbf9c83949387a109faa3
Nicolás <nicoulmete1@gmail.com>
2020-09-13 21:19:41 -0300

    use real domain 2.0

--- src/mixins/images.js
+++ src/mixins/images.js
@@ -3,7 +3,7 @@ export default {
     imagePath (image) {
       // image: /testimonio-1-640w-min.jpg
       // dev: `@/assets/img/${image}`
-      // prod: `...cloudfront.net/img/${image}`
+      // prod: `https://ojosdelalma.com/img/${image}`
       if (process.env.NODE_ENV === 'production') {
         return `${process.env.VUE_APP_IMG_PATH}${image}`
       } else {
